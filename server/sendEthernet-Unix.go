f29def2a2315ad806e025529327a53686155d65c
Joe Wolfgram <joe.wolfgram@icloud.com>
2026-03-23 11:16:31 -0400

    patch fix for not having serverIPAddr set

--- server/sendEthernet-Unix.go
+++ server/sendEthernet-Unix.go
@@ -14,6 +14,31 @@ import (
 	"github.com/insomniacslk/dhcp/dhcpv4"
 )
 
+func getInterfaceIPv4(iface net.Interface) (net.IP, error) {
+	addrs, err := iface.Addrs()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, addr := range addrs {
+		// addr is typically a *net.IPNet or *net.IPAddr
+		var ip net.IP
+		switch v := addr.(type) {
+		case *net.IPNet:
+			ip = v.IP
+		case *net.IPAddr:
+			ip = v.IP
+		}
+
+		// Check if it's a valid IPv4 address (not IPv6) and not loopback
+		if ip != nil && !ip.IsLoopback() && ip.To4() != nil {
+			return ip.To4(), nil
+		}
+	}
+
+	return nil, fmt.Errorf("no IPv4 address found for interface %s", iface.Name)
+}
+
 func sendEthernet(iface net.Interface, resp *dhcpv4.DHCPv4) error {
 	f, err := findBPFDevice()
 	if err != nil {
@@ -26,6 +51,17 @@ func sendEthernet(iface net.Interface, resp *dhcpv4.DHCPv4) error {
 	_, _, errno := syscall.Syscall(syscall.SYS_IOCTL, f.Fd(), uintptr(syscall.BIOCSETIF), uintptr(unsafe.Pointer(&ifr[0])))
 	if errno != 0 {
 		return fmt.Errorf("ioctl BIOCSETIF failed: %v", errno)
+	}
+
+	if resp.ServerIPAddr == nil || resp.ServerIPAddr.IsUnspecified() {
+		// I think this is the hacky way of doing this but sometimes the `SrcIP:    resp.ServerIPAddr,`
+		// is correct for the interface and sometimes it is 0.0.0.0:68 causing issues with DHCP clients that
+		// want this presion. So lets hack it in here to make it relible, which is better then a bad fix this might be
+		localIP, err := getInterfaceIPv4(iface) // Use the helper function from earlier
+		if err != nil {
+			return err
+		}
+		resp.ServerIPAddr = localIP
 	}
 
 	eth := layers.Ethernet{
